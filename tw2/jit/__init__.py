d08a87d1e858760e5d2afd07556ca38986064943
Ralph Bean <ralph.bean@gmail.com>
2010-08-06 11:36:40 -0400

    Cleaned up __init__.py

--- tw2/jit/__init__.py
+++ tw2/jit/__init__.py
@@ -5,17 +5,6 @@ This package contains the basic jit widgets.
 # TODO -- is this the right way todo this?
 jit_base = "static/Jit-2.0.0a"
 
-#from widgets import (AreaChart, BarChart, PieChart,
-#                     TreeMap, ForceDirectedGraph, RadialGraph,
-#                     Sunburst, Icicle, SpaceTree)
-from widgets import AreaChart
-
-"""
-from widgets import (Button, CheckBox, FieldSet, FileField, Form, HiddenField, ImageButton,
-    Label, Spacer, ListLayout, TableLayout, PasswordField,
-    RadioButton, ResetButton, SubmitButton, TextField, TextArea,
-    SingleSelectField, MultipleSelectField, RadioButtonList, CheckBoxList,
-    RadioButtonTable, CheckBoxTable, GridLayout, RowLayout, TableForm, ListForm,
-    TableFieldSet, ListFieldSet, FormPage, FileValidator,
-    LabelField, LinkField, InputField, SelectionField, MultipleSelectionField)
-"""
+from widgets import (AreaChart, BarChart, PieChart,
+                     TreeMap, ForceDirectedGraph, RadialGraph,
+                     Sunburst, Icicle, SpaceTree, HyperTree)
