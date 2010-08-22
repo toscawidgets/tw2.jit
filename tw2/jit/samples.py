d19dd4e584a315ea209ca89b3081ee0802a71241
Ralph Bean <ralph.bean@gmail.com>
2010-08-22 18:21:21 -0400

    got force directed working with sample

--- tw2/jit/samples.py
+++ tw2/jit/samples.py
@@ -21,9 +21,15 @@ class DemoPieChart(PieChart):
     data = PieChartJSONDefaults
     sliceOffset = 5 
 
+
+from tw2.jit.widgets import RadialGraph
+from tw2.jit.defaults import RadialGraphJSONDefaults
+
 class DemoRadialGraph(RadialGraph):
     data = RadialGraphJSONDefaults
+
     background = { 'CanvasStyles':{ 'strokeStyle' : '#555' } }
+
     postInitJSCallback = JSSymbol(src="""
         (function (jitwidget) {
             //trigger small animation for kicks
@@ -76,5 +82,115 @@ class DemoRadialGraph(RadialGraph):
             var w = domElement.offsetWidth;
             style.left = (left - w / 2) + 'px';
         })""")
+
+
+from tw2.jit.widgets import ForceDirectedGraph
+from tw2.jit.defaults import ForceDirectedGraphJSONDefaults
+
+class DemoForceDirectedGraph(ForceDirectedGraph):
+    data = ForceDirectedGraphJSONDefaults
+
+    iterations = 25
+
+    levelDistance = 75
+
+    postInitJSCallback = JSSymbol(src=""" 
+        (function (jitwidget) {
+            jitwidget.computeIncremental({
+                iter: 40,
+                property: 'end',
+                onComplete: function(){
+                    jitwidget.animate({
+                        modes: ['linear'],
+                        transition: $jit.Trans.Elastic.easeOut,
+                        duration: 2500
+                    });
+                }
+            });
+        })""")
+
+    Navigation = {
+        'enable' : True,
+        'panning' : 'avoid nodes',
+        'zooming' : 10
+    }
+    Node = {
+        'overridable' : True,
+    }
+    Edge = {
+        'overridable' : True,
+        'color' : '#23A4FF',
+        'lineWidth' : 0.4,
+    }
+    Label = {
+        'style' : 'bold',
+    }
+    Tips = {
+        'enable' : True,
+        'onShow' : JSSymbol(src="""
+            (function(tip, node) {
+                var count = 0;
+                node.eachAdjacency(function() { count++; });
+                tip.innerHTML = '<div class="tip-title">' 
+                    + node.name + '</div>'
+                    + '<div class="tip-text"><b>connections:</b> ' 
+                    + count + '</div>';
+            })"""),
+    }
+
+    Events = {
+            'enable' : True,
+            'onMouseEnter' : JSSymbol(src="""
+            (function() { 
+                jitwidget.canvas.getElement().style.cursor = \'move\';
+            })"""),
+            'onMouseLeave' : JSSymbol(src="""
+            (function() {
+                jitwidget.canvas.getElement().style.cursor = \'\';
+            })"""),
+            'onDragMove' : JSSymbol(src="""
+            (function(node, eventInfo, e) {
+                var pos = eventInfo.getPos();
+                node.pos.setc(pos.x, pos.y);
+                jitwidget.plot();
+            })"""),
+            'onTouchMove' : JSSymbol(src="""
+            (function(node, eventInfo, e) {
+                $jit.util.event.stop(e);
+                this.onDragMove(node, eventInfo, e);
+            })"""),
+            'onClick' : JSSymbol(src="""
+            (function(node) {
+                if(!node) return;
+                var html = "<h4>" + node.name + "</h4><b> connections:</b><ul><li>",
+                list = [];
+                node.eachAdjacency(function(adj){
+                    list.push(adj.nodeTo.name);
+                });
+                alert("this is connected to: " + list.join(", "));
+            })""")
+    }
+    onCreateLabel = JSSymbol(src="""
+        (function(domElement, node){
+              domElement.innerHTML = node.name;
+              var style = domElement.style;
+              style.fontSize = "0.8em";
+              style.color = "#ddd";
+        })""" )
+    onPlaceLabel = JSSymbol(src="""
+        (function(domElement, node){
+            var style = domElement.style;
+            var left = parseInt(style.left);
+            var top = parseInt(style.top);
+            var w = domElement.offsetWidth;
+            style.left = (left - w / 2) + 'px';
+            style.top = (top + 10) + 'px';
+            style.display = '';
+        })""")
+
+    
+
+
+
 
 
