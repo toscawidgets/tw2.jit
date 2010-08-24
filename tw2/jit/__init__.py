66ef0daa29d1c6da781b623ff7804f70929b81a7
Ralph Bean <ralph.bean@gmail.com>
2010-08-24 00:48:24 -0400

    now theres a conflict between widgets on the same page.  gotta work it out.

--- tw2/jit/__init__.py
+++ tw2/jit/__init__.py
@@ -5,7 +5,6 @@ This package contains the basic jit widgets.
 #TODO -- this is *not* the right way to do this.
 jit_base = "static/Jit-2.0.0a"
 
-#from widgets import (AreaChart, BarChart, PieChart,
-#                     TreeMap, ForceDirectedGraph, RadialGraph,
-#                     Sunburst, Icicle, SpaceTree, HyperTree)
-from widgets import Sunburst 
+from widgets import (AreaChart, BarChart, PieChart,
+                     TreeMap, ForceDirectedGraph, RadialGraph,
+                     Sunburst, Icicle, SpaceTree, HyperTree)
