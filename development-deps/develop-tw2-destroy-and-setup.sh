f2c10934bcf49beae95b6c85d55b4468706cf80b
Ralph Bean <ralph.bean@gmail.com>
2011-03-01 16:10:13 -0500

    using local devtools

--- development-deps/develop-tw2-destroy-and-setup.sh
+++ development-deps/develop-tw2-destroy-and-setup.sh
@@ -12,11 +12,11 @@ source $venv/bin/activate
 pushd $devbase
 hg clone http://bitbucket.org/paj/tw2core || echo "tw2core exists."
 hg clone http://bitbucket.org/paj/tw2forms || echo "tw2forms exists."
-hg clone http://bitbucket.org/ralphbean/tw2devtools || echo "tw2devtools exists."
+#hg clone http://bitbucket.org/ralphbean/tw2.devtools || echo "tw2devtools exists."
 hg clone http://bitbucket.org/toscawidgets/tw2jquery || echo "tw2jquery exists."
 pushd tw2core;hg pull;python setup.py develop;popd
 pushd tw2forms;hg pull;python setup.py develop;popd
-pushd tw2devtools;hg pull;python setup.py develop;popd
+pushd ../../tw2.devtools;hg pull;python setup.py develop;popd
 pushd tw2jquery;hg pull;python setup.py develop;popd
 popd
 
--- development-deps/develop-tw2-start.sh
+++ development-deps/develop-tw2-start.sh
@@ -5,6 +5,8 @@ venv=$devbase/virtualenv-tw2.jit
 source $venv/bin/activate
 pip install formencode
 
+pushd ../../tw2.devtools;hg pull;python setup.py develop;popd
+
 python setup.py develop && paster tw2.browser
 
 
