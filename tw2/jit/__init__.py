2b83294a3549e75f50e2a3d07e08a81c15421d33
Ralph Bean <ralph.bean@gmail.com>
2011-05-17 16:21:48 -0400

    Upgraded to latest version of thejit.

--- tw2/jit/__init__.py
+++ tw2/jit/__init__.py
@@ -5,7 +5,7 @@ Get this source from http://github.com/ralphbean/tw2.jit
 """
 
 #TODO -- this is *not* the right way to do this.
-jit_base = "static/Jit-2.0.0a"
+jit_base = "static/Jit-2.0.1"
 
 from widgets import (AreaChart, BarChart, PieChart,
                      TreeMap, ForceDirectedGraph, RadialGraph,
