00097a3039bae2e6f3435d86982f92035edd1875
Ralph Bean <rbean@redhat.com>
2012-03-20 22:34:11 -0400

    Port forwards to tw2 2.0.0

--- tw2/jit/__init__.py
+++ tw2/jit/__init__.py
@@ -7,9 +7,18 @@ Get this source from http://github.com/toscawidgets/tw2.jit
 #TODO -- this is *not* the right way to do this.
 jit_base = "static/Jit-2.0.1"
 
-from widgets import (AreaChart, BarChart, PieChart,
-                     TreeMap, ForceDirectedGraph, RadialGraph,
-                     Sunburst, Icicle, SpaceTree, HyperTree)
+from widgets import (
+    AreaChart,
+    BarChart,
+    PieChart,
+    TreeMap,
+    ForceDirectedGraph,
+    RadialGraph,
+    Sunburst,
+    Icicle,
+    SpaceTree,
+    HyperTree,
+)
 
 from widgets import AjaxRadialGraph
 
